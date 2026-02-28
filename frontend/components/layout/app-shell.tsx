1cc95fc280ce1363f4777752c9a2a10a2a20683c
ramgawas55 <ramgawas55@gmail.com>
2026-03-01 01:10:21 +0530

    add-use-client-to-client-components

--- frontend/components/layout/app-shell.tsx
+++ frontend/components/layout/app-shell.tsx
@@ -1,3 +1,4 @@
+use client;
 import * as React from "react";
 import { Sidebar } from "./sidebar";
 import { Topbar } from "./topbar";
--- frontend/components/layout/sidebar.tsx
+++ frontend/components/layout/sidebar.tsx
@@ -1,3 +1,4 @@
+use client;
 import Link from "next/link";
 import { currentUser } from "../../lib/mock";
 import { Role } from "../../lib/types";
